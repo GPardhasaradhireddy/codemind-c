89404fbd2450cd336f01b0af8ea6c5808edfb819
GPardhasaradhireddy <147792497+GPardhasaradhireddy@users.noreply.github.com>
2023-12-07 22:47:17 +0530

    #include<stdio.h> int main() {     int n1,n2,n3,n4,n5,n6,n7;     scanf("%d%d%d%d%d%d%d",&n1,&n2,&n3,&n4,&n5,&n6,&n7);     if(n3-n4>=n2)     printf("YES ");     else     printf("NO ");     if(n6-n7>=n5)     printf("YES ");     else     printf("NO"); }

---
+++ Student_Registration.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,n3,n4,n5,n6,n7;
+    scanf("%d%d%d%d%d%d%d",&n1,&n2,&n3,&n4,&n5,&n6,&n7);
+    if(n3-n4>=n2)
+    printf("YES
+");
+    else
+    printf("NO
+");
+    if(n6-n7>=n5)
+    printf("YES
+");
+    else
+    printf("NO");
+}
